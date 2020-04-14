fcffa32d1f9876135dd61636c9434e760b6e425d
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-04-14 22:10:24 +0100

    défi1

---
+++ Défi_1/app.js
@@ -0,0 +1,17 @@
+const xhr = new XMLHttpRequest;
+const button = document.getElementById('button');
+
+button.addEventListener('click',function (){
+    xhr.onload = function (){
+        if (xhr.readyState == 4){
+            if (xhr.status == 200) {
+                document.getElementById('output').innerHTML = xhr.responseText;
+            }
+            if (xhr.status == 404) {
+                console.log("ERROR 404")
+            }
+        }
+    }
+    xhr.open('get', 'data.txt', true);
+    xhr.send();
+})
